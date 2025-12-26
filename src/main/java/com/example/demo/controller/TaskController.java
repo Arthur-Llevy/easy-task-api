5ccf3f1b523e8a941445c9af067bf9b319b03a7f
Arthur Levy <arthurlbvieiraa@gmail.com>
2025-12-26 17:21:31 -0300

    feat: implemented database

--- src/main/java/com/example/demo/controller/TaskController.java
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -1,15 +1,22 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.DTO.request.TaskRequestDTO;
+import com.example.demo.DTO.request.TaskUpdateRequestDTO;
+import com.example.demo.exception.TaskNotFoundException;
 import com.example.demo.model.Task;
 import com.example.demo.service.TaskService;
 
@@ -28,10 +35,36 @@ public class TaskController {
         return ResponseEntity.ok(tasks);
     }
 
+    @GetMapping("/{id}")
+    // get - /api/taks
+    public ResponseEntity<Optional<Task>> getTaskById(@PathVariable int id) {
+        Optional<Task> task = taskService.getById(id);
+
+        return ResponseEntity.ok(task);
+    }
+
     @PostMapping
     public ResponseEntity<Task> createNewTask(@RequestBody TaskRequestDTO taskRequestDTO) {
         Task createdTask = taskService.createTask(taskRequestDTO);
 
         return ResponseEntity.ok(createdTask);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTaskById(@PathVariable int id, @RequestBody TaskUpdateRequestDTO taskUpdateRequestDTO) {
+        try {
+            Task updatedTask = taskService.updateTask(taskUpdateRequestDTO, id);
+            return ResponseEntity.status(HttpStatus.OK).body(updatedTask);
+
+        } catch (TaskNotFoundException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTaskById(@PathVariable int id) {
+        taskService.delteById(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
     }
 }
--- src/main/java/com/example/demo/service/TaskService.java
+++ src/main/java/com/example/demo/service/TaskService.java
@@ -1,10 +1,13 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.example.demo.DTO.request.TaskRequestDTO;
+import com.example.demo.DTO.request.TaskUpdateRequestDTO;
+import com.example.demo.exception.TaskNotFoundException;
 import com.example.demo.model.Task;
 import com.example.demo.repository.TaskRepository;
 
@@ -21,11 +24,35 @@ public class TaskService {
         return tasks;
     }
 
+    public Optional<Task> getById(int id) {
+        Optional<Task> task = taskRepository.findById(id);
+
+        return task;
+    }
+
     public Task createTask(TaskRequestDTO taskRequestDTO) {
         Task taskToCreate = new Task();
         taskToCreate.setTitle(taskRequestDTO.getTitle());
         taskToCreate.setCompleted(taskRequestDTO.getCompleted());
 
         return taskRepository.save(taskToCreate);
+    }
+
+    public Task updateTask(TaskUpdateRequestDTO taskUpdateRequestDTO, int id) {
+        Optional<Task> taskToUpdate = taskRepository.findById(id);
+        
+        if (taskToUpdate.isEmpty()) {
+            throw new TaskNotFoundException("Task not foud with this id");
+        }
+
+        taskToUpdate.get().setTitle(taskUpdateRequestDTO.getTitle().isEmpty() ? taskToUpdate.get().getTitle() : taskUpdateRequestDTO.getTitle());
+        taskToUpdate.get().setCompleted(taskUpdateRequestDTO.getCompleted() != null ? taskUpdateRequestDTO.getCompleted() : taskToUpdate.get().getCompleted());
+
+        return taskRepository.save(taskToUpdate.get());
+    }
+
+    public void delteById(int id) {
+        Optional<Task> task = taskRepository.findById(id);
+        taskRepository.delete(task.get());
     }
 }
